184a1ac359b5c0181e133f21a0202d57d34995aa
sonu singh <sonusin3435@gmail.com>
2023-05-13 07:30:02 +0530

    complete Discord With response

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+  plugins: [],
+}
+
